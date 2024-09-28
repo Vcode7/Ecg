5b615f98df0d8b92a1bf1134e1770c03b50f1f74
Vikas <90235735+Vcode7@users.noreply.github.com>
2024-09-28 09:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/data', async (req, res) => {
 });
 
 // Start the server
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server running on port ${PORT}');
 });
